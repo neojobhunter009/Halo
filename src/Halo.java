956f2b907d945436fd24aebd854108e3a25829d3
neojobhunter009 <neojobhunter009@gmail.com>
2023-06-02 08:19:28 +0530

    First project repo with eclipse

---
+++ src/Halo.java
@@ -0,0 +1,11 @@
+
+public class Halo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Eclipse!"); 
+		
+		byte b  = (byte)180;
+		System.out.println(b);
+	}
+
+}
